f25fd15341b3984b6751e370980ee83ccb2b3657
ayki1 <ayhankaykac@gmail.com>
2023-03-29 22:59:02 +0300

    kacış dizeleri

---
+++ src/main/javaKodlar/ders1_JavaTanitim/kacisDizeleri.java
@@ -0,0 +1,17 @@
+package ders1_JavaTanitim;
+
+public class kacisDizeleri {
+    public static void main(String[] args) {
+        //Escape sequences
+
+        System.out.println("Merhaba Dünya");
+
+        System.out.println("Merhaba\tDünya");
+        System.out.println("AD:\nSOYAD:");
+        System.out.println("Yeni keşif edilen gezegenin adı \"Kripton\" galiba");
+        System.out.println("İstanbul\\Üsküdar");
+
+        System.err.println("Merhaba Dünya");
+
+    }
+}
