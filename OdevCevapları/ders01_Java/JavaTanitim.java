781f52ef3ae6e157a8f49c67fb30c4f2369d4eeb
ayki1 <ayhankaykac@gmail.com>
2023-04-03 10:46:57 +0300

    odev1

---
+++ OdevCevapları/ders01_Java/JavaTanitim.java
@@ -0,0 +1,13 @@
+package ders01_Java;
+
+public class JavaTanitim {
+
+    //Konsola "Merhaba" yazdır
+    public static void main(String[] args) {
+        //System.out.println("Merhaba");
+        System.out.print("one");
+        System.out.print("two");
+        System.out.println("three");
+        System.out.print("three");
+    }
+}
